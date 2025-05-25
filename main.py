4eb1e010ea8784353197568a339b4defc9f4876d
Conrado1600 <conradoabud22@gmail.com>
2025-05-25 11:17:31 -0300

    1.25/05

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from Vehiculo.vehiculo import Vehiculos_terrestre, Helicoptero, Avion
 
 incucai = INCUCAI()
 
-centro1 = Centro_salud("Hospital Pergamino", "Liniers 979", "Pergamino", "Buenos Aires", "02477 – 429792")
+centro1 = Centro_salud("Hospital Zonal Pergamino", "Liniers 979", "Pergamino", "Buenos Aires", "02477 – 429792")
 centro2 = Centro_salud("Hospital Blas L. Dubarry", "Calle 12 nro 825", "Mercedes", "Buenos Aires", "02324 - 425555")
 
 centro1.agregar_vehiculo(Vehiculos_terrestre(100))
@@ -21,9 +21,10 @@ centro2.agregar_vehiculo(Avion(580))
 incucai.centros_salud.append(centro1)
 incucai.centros_salud.append(centro2)
 
-ORGANOS_VALIDOS = ["corazón", "hígado", "pancreas", "hueso", "riñon", "pulmones", "intestino", "piel", "córneas"]
-TIPOS_SANGRE = ["A+", "A-","B+", "B-","AB+", "AB-","O+", "O-"]#preguntar sobre rh nulo
-#PRIORIDAD = ["Alta", "Media", "Baja"]
+organos_validos = ["corazón", "hígado", "pancreas", "hueso", "riñon", "pulmones", "intestino", "piel", "córneas"]
+tipos_sangre = ["A+", "A-","B+", "B-","AB+", "AB-","O+", "O-"]#preguntar sobre rh nulo
+prioridad = ["Alta", "Media", "Baja"]
+sexo_m_f = ["Masculino", "Femenino"]
 
 def seleccionar_opcion(lista, mensaje= "Selecciones una opcion:"): #funcion enumerate para facilitar entrada de usuario
     while True:
@@ -31,15 +32,15 @@ def seleccionar_opcion(lista, mensaje= "Selecciones una opcion:"): #funcion enum
         for i, item in enumerate(lista, 1):
             print(f"{i}. {item}")
         try:
-            opcion = int(input("Opcion: 1"))
+            opcion = int(input("Opcion: "))
             if 1 <= opcion <= len(lista):
                 return lista[opcion - 1]
             else:
-                print ("Opcion fuera de rango")
+                print ("Opcion inválida.")
         except ValueError:
             print("Entrada inválida. Ingrese un número.")
 
-def seleccionar_multiples_opciones(lista, mensaje = "Seleccione elementos (0 para terminar):"):
+def seleccionar_multiples_opciones(lista, mensaje = "Seleccione una opción (0 para finalizar):"):#funcion enumerate
     seleccionados = []
     while True:
         print(mensaje)
@@ -52,12 +53,12 @@ def seleccionar_multiples_opciones(lista, mensaje = "Seleccione elementos (0 par
             if 1 <= opcion <= len(lista):
                 seleccionados.append(lista[opcion - 1])
             else: 
-                print("Opción fuera de rango.")
+                print("Opción inválida.")
         except ValueError:
-            print("Entrada incorrecta.")
+            print("Respuesta incorrecta.")
     return seleccionados
 
-def manu ():
+def menu ():
     while True: 
         print("\\n ----🦾🫀Sistema de Donanción y Transplante🫀🦾----")
         print("1️⃣. Registrar nuevo paciente")
@@ -73,22 +74,25 @@ def manu ():
         
 def registrar_paciente():
 
-    tipo = input("¿Es Donante (D) o Receptor (R)?").upper()
-    if tipo not in ["D", "R"]:
-        print("Opcion incorrecta.")
-        return
+    tipo_persona = seleccionar_opcion(["Donante", "Receptor"], "Seleccione el tipo de paciente: ")
+    tipo = "D" if tipo_persona == "Donante" else "R"
     try:
         nombre = input("Nombre: ")
         dni = int(input("DNI: ")) #es un nro entero
-        fecha_nacimiento = datetime.strptime(input("Fecha de nacimiento (DD-MM-AAAA): "), "%d-%m-%A" )
-        sexo = input("Sexo: (M/F):").upper()
+        fecha_nacimiento = datetime.strptime(input("Fecha de nacimiento (DD-MM-AAAA): "), "%d-%m-%A" )}
+        sexo_opcion = seleccionar_opcion (sexo_m_f, "Seleccione sexo.")
+        sexo = "M" if sexo_opcion == "Masculino" else "F"
         telefono = input("Teléfono: ")
-        tipo_sangre = seleccionar_opcion(TIPOS_SANGRE, "Seleccione tipo de sangre: ")
+        tipo_sangre = seleccionar_opcion(tipos_sangre, "Seleccione tipo de sangre: ")
 
     print("Centros disponibles:")
     for i, c in enumerate (incucai.centros_salud):
         print(f"{i + 1}. {c.nombre} ({c.partido}, {c.provincia})")
-    centro = incucai.centros_salud[int(input("Seleccione centro (numero): ")) - 1]
+    index =  int(input("Seleccione centro (número): "))-1
+    if not 0 <= index < len(incucai.centros_salud):
+        print ("El centro de salud ingresado es incorrecto")
+        return 
+    centro = incucai.centros_salud[index]
 
 
     if tipo == "D":
